b9dd61565363ef0011c9d369001debf063b19150
Anne Kimsey <anne.kimsey@gmail.com>
2013-10-29 15:00:23 -0700

    Added downvote option. No negatives yet though.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,20 @@
 class VotesController < ApplicationController
   def create
-    topic = Topic.find(params[:topic_id])
+  	topic = Topic.find(params[:topic_id])
     vote = topic.votes.build
     vote.save!
     redirect_to(topics_path)
+  end
+
+  # DELETE /topics/1
+  # DELETE /topics/1.json
+    def destroy
+    vote = Vote.find(params[:id])
+    vote.destroy
+
+    respond_to do |format|
+      format.html { redirect_to topics_url }
+      format.json { head :no_content }
+    end
   end
 end
